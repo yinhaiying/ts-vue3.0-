0169c0a645f167776b0e174e048b1c76e1c72e71
yinhaiying <18840842571@163.com>
2020-12-10 07:52:46 +0800

    ref和computed的使用

---
+++ packages/vue3.0-basis/src/components/ref-computed.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="hello">
+    <h1>Vue3.0</h1>
+    <h1>{{count}}</h1>
+    <h1>{{double}}</h1>
+    <button @click = "increase">+</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent,ref,computed } from 'vue';
+
+export default defineComponent({
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+  setup(){
+    const count = ref(0);
+    const increase = () => {
+      count.value += 1;
+    }
+    const double = computed(() => {
+      return count.value * 2;
+    })
+    return {
+      count,
+      increase,
+      double
+    }
+  }
+});
+</script>
+
