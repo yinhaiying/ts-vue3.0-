55c4c1464b1b879977f12e64fcdabd095317a3d9
yinhaiying <yinhaiying@58.com>
2020-12-11 17:45:17 +0800

    引入vue-router

---
+++ packages/zhihu/src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="page">Home主页</div>
+</template>
+
+<script>
+import {defineComponent} from "vue";
+export default defineComponent({
+    name:"Home"
+});
+</script>
+<style lang="less" scoped>
+
+</style>
---
+++ packages/zhihu/src/views/Login.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="page">Login</div>
+</template>
+
+<script>
+import {defineComponent} from "vue";
+export default defineComponent({
+    name:"Login"
+})
+</script>
+<style lang="less" scoped>
+</style>
