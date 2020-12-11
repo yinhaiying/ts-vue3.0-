990a10e1d773b6ffcdadbc70c5383ec4d796d58a
yinhaiying <18840842571@163.com>
2020-12-11 08:27:39 +0800

    columnList组件的编写

--- packages/zhihu/src/App.vue
+++ packages/zhihu/src/App.vue
@@ -1,16 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-
+  <div class="container">
+    <column-list :list="list"></column-list>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-
+import { defineComponent } from "vue";
+import ColumnList, { ColumnProps } from "./components/ColumnList.vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    avatar: "https://pic4.zhimg.com/v2-675b5f63b8fda663cc12cc95aec4f5b6_xs.jpg",
+    title: "海鹰的专栏",
+    description: "这是海鹰的专栏，主要是一些前端相关的工作",
+  },
+  {
+    id: 2,
+    avatar: "https://pic1.zhimg.com/v2-a8faa37c113eff2c80e01c91dff5890b_xs.jpg",
+    title: "海鹰的专栏",
+    description: "这是海鹰的专栏，主要是一些前端相关的工作",
+  },
+  {
+    id: 3,
+    avatar: "https://pic1.zhimg.com/v2-56fdabb5e6c6386e61b037261627fc5c_xs.jpg",
+    title: "海鹰的专栏",
+    description: "这是海鹰的专栏，主要是一些前端相关的工作",
+  },
+  {
+    id: 4,
+    title: "海鹰的专栏",
+    description: "这是海鹰的专栏，主要是一些前端相关的工作",
+  },
+];
 export default defineComponent({
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
+    ColumnList,
+  },
+  setup() {
+    return {
+      list: testData,
+    };
+  },
 });
 </script>
 
